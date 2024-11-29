b40631baeea01f28e7d83557c68382eaf30927c1
dialgi <128111454+dialgi@users.noreply.github.com>
2024-11-29 09:20:48 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
 import ErrorRoute from "./pages/errorRoute";
-import ForgotPasswordPage from "./pages/forgotPassword";
+import ForgotPasswordPage from "./pages/ForgotPassword";
 import DashboardPage from "./pages/dashboard";
 import BalancePage from "./pages/balance";
 import GoalPage from "./pages/goal";
